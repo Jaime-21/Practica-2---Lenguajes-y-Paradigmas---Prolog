022bd5163e4eeeb4d26f0c67addc1b5de86b085b
Matias Zapata Rojas <matiaszptro@gmail.com>
2025-08-30 08:47:12 -0500

    Errores de mayusculas corregidos

--- Practica.pl
+++ Practica.pl
@@ -1,28 +1,30 @@
-vehicle(Brand, Reference, Type, Price, Year).
-
-vehicle(Toyota, Land Cruiser Prado, SUV, 65000, 2023).
-vehicle(Toyota, Corolla, compact car, 20000, 2024).
-vehicle(Toyota, Hilux, pickup, 50000, 2025).
-vehicle(Toyota, Camry, Sedan, 30000, 2023).
-vehicle(toyota, Supra, sport, 45000, 2021). 
-
- 
-vehicle(Ford, Raptor, pickup, 80000, 2022).
-vehicle(Ford, Ranger, pickup, 33000, 2023).
-vehicle(Ford, Mustang, sport, 47000, 2023).
-vehicle(Ford, Bronco, SUV, 50000, 2019).
-vehicle(Ford, Escape, SUV, 35000, 2025).
+% -- vehicle(Brand, Reference, Type, Price, Year).
 
 
-vehicle(BMW, x3, SUV, 50000, 2022).
-vehicle(BMW, x5, SUV, 60000, 2021)
-vehicle(BMW, m4, sport, 72000, 2023).
-vehicle(BMW, m6, sport, 90000, 2024).
-vehicle(BMW, i5, sedan, 68000, 2025).
+% -- Toyota
+vehicle(toyota, 'land cruiser prado', suv, 65000, 2023).
+vehicle(toyota, corolla, 'compact car', 20000, 2024).
+vehicle(toyota, hilux, pickup, 50000, 2025).
+vehicle(toyota, camry, sedan, 30000, 2023).
+vehicle(toyota, supra, sport, 45000, 2021). 
 
+% -- Ford
+vehicle(ford, raptor, pickup, 80000, 2022).
+vehicle(ford, ranger, pickup, 33000, 2023).
+vehicle(ford, mustang, sport, 47000, 2023).
+vehicle(ford, bronco, suv, 50000, 2019).
+vehicle(ford, escape, suv, 35000, 2025).
 
-vehicle(Chevrolet, Spark, sedan, 15000, 2020).
-vehicle(Chevrolet, Tracker, SUV, 26000, 2021).
-vehicle(Chevrolet, Silverado, pickup, 40000, 2022).
-vehicle(Chevrolet, Camaro, sport, 42000, 2021).
-vehicle(Chevrolet, Traverse, suv, 35000, 2023).
+% -- BMW
+vehicle(bmw, x3, suv, 50000, 2022).
+vehicle(bmw, x5, suv, 60000, 2021).
+vehicle(bmw, m4, sport, 72000, 2023).
+vehicle(bmw, m6, sport, 90000, 2024).
+vehicle(bmw, i5, sedan, 68000, 2025).
+
+% -- Chevrolet
+vehicle(chevrolet, spark, sedan, 15000, 2020).
+vehicle(chevrolet, tracker, suv, 26000, 2021).
+vehicle(chevrolet, silverado, pickup, 40000, 2022).
+vehicle(chevrolet, camaro, sport, 42000, 2021).
+vehicle(chevrolet, traverse, suv, 35000, 2023).
