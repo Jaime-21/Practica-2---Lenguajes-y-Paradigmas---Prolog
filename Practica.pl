825f2ef07a60b1166af811de738b0c64746ae5fe
Matias Zapata Rojas <matiaszptro@gmail.com>
2025-08-29 21:45:04 -0500

    Primeros carros agregados e inicio del codigo

---
+++ Practica.pl
@@ -0,0 +1,17 @@
+vehicle(Brand, Reference, Type, Price, Year).
+
+vehicle(Toyota, Land Cruiser Prado, suv, 65000, 2023).
+vehicle(Toyota, Corolla, compact car, 20000, 2024).
+vehicle(Toyota, Hilux, pickup truck, 50000, 2025).
+vehicle(Toyota, Camry, Sedan, 30000, 2023).
+vehicle(toyota, supra, sport, 45000, 2021). 
+
+ 
+vehicle(Ford, Raptor, pickup truck, 80000, 2022).
+vehicle(ford, ranger, pickup, 33000, 2023).
+vehicle(ford, mustang, sport, 47000, 2023).
+
+
+vehicle(bmw, x3, suv, 50000, 2022).
+vehicle(bmw, x5, suv, 60000, 2021)
+vehicle(bmw, m4, sport, 72000, 2023).
